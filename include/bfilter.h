528bf431e0acb147870721531a9bfe6475972d8d
Laurie Harvey <laurieharvey1602@gmail.com>
2021-01-31 18:41:58 +0000

    Add insert and contains api

--- include/bfilter.h
+++ include/bfilter.h
@@ -1,18 +1,67 @@
+#include <functional>
+#include <vector>
+
 namespace bloom
 {
-    template <typename Key>
+    template <class T>
+    struct hash
+    {
+        std::size_t seed;
+
+        inline std::size_t operator()(const T &v)
+        {
+            return seed ^ std::hash<T>{}(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+        }
+    };
+
+    template <typename Value>
     class bfilter
     {
+        std::vector<bool> filter_;
+        int hashes_;
+
     public:
-        using key_type = Key;
-        using value_type = Key;
+        using value_type = Value;
+        using size_type = std::vector<bool>::size_type;
 
-        explicit bfilter();
-
-        void insert(const value_type &value);
-
-        bool contains(const key_type &key) const;
-
-        bool empty() const;
+        explicit bfilter(int hashes, size_type size)
+            : hashes_(hashes), filter_(size, false)
+        {
+        }
+        /*
+         * Insert value into the filter
+         */
+        void insert(const value_type &value)
+        {
+            for (unsigned int i = 0; i < hashes_; ++i)
+            {
+                filter_[hash<value_type>{i}(value) % filter_.size()] = true;
+            }
+        }
+        /*
+         * Insert value into the filter
+         */
+        void insert(value_type &&value)
+        {
+            for (unsigned int i = 0; i < hashes_; ++i)
+            {
+                filter_[hash<value_type>{i}(value) % filter_.size()] = true;
+            }
+        }
+        /*
+         * Check whether the filter possibly contains value or whether
+         * it definitely doesn't
+         */
+        bool contains(const value_type &value) const
+        {
+            for (unsigned int i = 0; i < hashes_; ++i)
+            {
+                if (!filter_[hash<value_type>{i}(value) % filter_.size()])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     };
 } // namespace bloom

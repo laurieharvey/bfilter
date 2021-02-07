84f032ff611e80e5f2192c5a4dd36b98f6ac38b5
laurie <laurieharvey1602@gmail.com>
2021-02-07 15:00:29 +0000

    Add to utilities

--- include/utilities.h
+++ include/utilities.h
@@ -2,9 +2,35 @@
 
 namespace bloom
 {
-    template <typename CountType, typename SizeType>
-    constexpr int optimum_hash_number(SizeType size, CountType expected_elements)
+    /*
+     * Return the false positive probability for a given filter of size m, containing n 
+     * elements and using k independent hash functions
+     */
+    template <typename SizeType, typename HashNumberType>
+    constexpr double false_positive_probability(SizeType m, SizeType n, HashNumberType k)
     {
-        return std::round( ( size * std::log( 2 ) ) / expected_elements );
+        return std::pow(1 - std::pow(1 - 1 / m, k * n), k);
+    }
+    /*
+     * Return the number of independent hash functions k which minimise the false
+     * probability for expected element count n and filter size m
+     */
+    template <typename SizeType, typename HashNumberType>
+    constexpr HashNumberType optimum_hash_number(SizeType m, SizeType n)
+    {
+        auto k = (m * std::log(2)) / n;
+
+        return false_positive_probability(m, n, std::ceil(k)) < false_positive_probability(m, n, std::floor(k))
+                   ? std::ceil(k)
+                   : std::floor(k);
+    }
+    /*
+     * Return the required filter size m for expected element count n and desired false
+     * probability e
+     */
+    template <typename SizeType>
+    constexpr SizeType required_filter_size(SizeType n, double e)
+    {
+        return -(n * std::log(e) / std::pow(std::log(2), 2));
     }
 } // namespace bloom
